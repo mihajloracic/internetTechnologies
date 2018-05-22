89bd16a045352404c4e4d29ca069ed66036b25cf
mihajlo <mihajlo.rac@gmail.com>
2018-05-22 13:41:28 +0200

    Comments basic view/controller

--- src/main/java/mihajlo/exampleantony/it/controller/CommentController.java
+++ src/main/java/mihajlo/exampleantony/it/controller/CommentController.java
@@ -1,12 +1,14 @@
 package mihajlo.exampleantony.it.controller;
 
 import mihajlo.exampleantony.it.entity.Comment;
+import mihajlo.exampleantony.it.entity.CustomUserDetail;
+import mihajlo.exampleantony.it.entity.User;
+import mihajlo.exampleantony.it.entity.dto.CommentDTO;
 import mihajlo.exampleantony.it.service.CommentService;
+import mihajlo.exampleantony.it.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +17,19 @@ public class CommentController {
     @Autowired
     CommentService commentService;
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/comment",method = RequestMethod.GET)
     public List<Comment> getComments(@RequestParam("id") Long id){
         return commentService.getCommentsForPlace(id);
+    }
+
+    @RequestMapping(value="/comment",method = RequestMethod.POST)
+    public Comment addComment(@RequestBody CommentDTO commentDTO){
+        CustomUserDetail userDetail = (CustomUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.getUser(userDetail);
+        Comment comment = commentService.addComment(commentDTO,user);
+        return comment;
     }
 }
--- src/main/java/mihajlo/exampleantony/it/controller/UserController.java
+++ src/main/java/mihajlo/exampleantony/it/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,13 @@ public class UserController {
         user = userService.getUser((CustomUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         userService.hasRole(user,"SUPER_ADMIN");
         userService.addRoleAdmin(user,new Role("ADMIN"));
+    }
+
+    @GetMapping(value = "/user/get")
+    public User getLoggedInUser(){
+        CustomUserDetail userDetail = (CustomUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.getUser(userDetail);
+        return user;
     }
 
 }
