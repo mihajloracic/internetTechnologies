18c938b8a477a399f70c48e51fe01657d372bcc1
mihajlo <mihajlo.rac@gmail.com>
2018-06-04 17:56:37 +0200

    Small fix footer

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,11 +1,15 @@
 var app = angular.module('myApp', []);
 app.run(function($rootScope,$http) {
+    $rootScope.loggedIn = false;
     $http.get("/user/get?access_token="+localStorage.getItem("token")).then(function (response) {
         $rootScope.user = response.data
-        $rootScope.loggedIn = localStorage.getItem("logedIn")
-        console.log($rootScope.user);
+        $rootScope.loggedIn = true;
     });
 });
 app.controller('navbarcontroller', function($scope, $http,$rootScope) {
-
+    $scope.logout = function(){
+        $rootScope.loggedIn = false;
+        $rootScope.user = '';
+        localStorage.setItem("token",'')
+    }
 });
