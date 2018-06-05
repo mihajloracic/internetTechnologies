018dbb5dd98cd4ce6543d8be792d72d4d47e92c4
mihajlo <mihajlo.rac@gmail.com>
2018-06-05 22:31:25 +0200

    mock data fix, finall but now really

--- src/main/java/mihajlo/exampleantony/it/service/MockDataService.java
+++ src/main/java/mihajlo/exampleantony/it/service/MockDataService.java
@@ -112,6 +112,7 @@ public class MockDataService {
             userService.save(new User("Admin","123","admin.rac@gmail.com","mihajlo","racic","https://scontent.fvno3-1.fna.fbcdn.net/v/t1.0-9/14063896_10201914928712972_71179004568042813_n.jpg?_nc_cat=0&oh=6c60348611f95a3a2ac45b862009983e&oe=5BBEB9F5"));
             User adminUser = userRepositoryForTest.findByUsername("Admin").get(0);
             adminUser.setRoles(Arrays.asList(new Role("USER"), new Role("ADMIN"), new Role("SUPER_ADMIN")));
+
         }
     }
 }
